7fb702dfc3ba855bc86ed9317dc8654b4ef502a8
alex <earthhuman41@gmail.com>
2019-07-03 11:54:43 +0300

    Updates

---
+++ precache-manifest.f42bcc0e88e5e53c5782ad3028eb3ab0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97ce9ebac7c567dd620c202b553ab5f",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "9616b898e58f21385d4f",
+    "url": "/react_static-list-of-posts/static/css/main.793dad1c.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "9616b898e58f21385d4f",
+    "url": "/react_static-list-of-posts/static/js/main.e11e6852.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
